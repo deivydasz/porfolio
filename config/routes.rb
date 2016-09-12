c48dce018bc3952200d23b7236c6035c37088463
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-09-12 14:44:31 +0300

    Remove devise signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  get '*path' => redirect('/')
   # root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
